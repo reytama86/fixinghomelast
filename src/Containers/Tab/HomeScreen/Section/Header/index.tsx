33a9096a704a6cf434432fc7cb5b8c7f2602a347
reytama86 <tritama96@gmail.com>
2026-01-14 02:46:35 +0700

    finishhh

--- src/Containers/Tab/HomeScreen/Section/Header/index.tsx
+++ src/Containers/Tab/HomeScreen/Section/Header/index.tsx
@@ -1,21 +1,34 @@
-import React, {useContext} from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
-import {Logout} from 'iconsax-react-native';
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { Logout } from 'iconsax-react-native';
 import { AuthContext } from '@Context/AuthContext';
-import styles from './styles'
+import styles from './styles';
 
 const Header: React.FC = () => {
-  const {logout} = useContext(AuthContext);
+  const { logout, user } = useContext(AuthContext);
+
+  
+  const displayName =
+    user?.role === 'developer'
+      ? 'IOT Developer'
+      : user?.role === 'user'
+      ? 'Bapak Arik'
+      : 'Guest';
+
+  const avatarSource =
+    user?.role === 'developer'
+      ? require('@Assets/images/developer.jpg') 
+      : require('@Assets/images/userr.png');     
 
   return (
     <View style={styles.header}>
       <TouchableOpacity onPress={() => {}} style={styles.userInfo}>
         <Image
-          source={require('@Assets/images/userr.png')}
+          source={avatarSource}
           style={styles.avatar}
           resizeMode="cover"
         />
-        <Text style={styles.username}>Bapak Arik</Text>
+        <Text style={styles.username}>{displayName}</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => logout()}>
@@ -23,7 +36,7 @@ const Header: React.FC = () => {
           color="black"
           variant="Outline"
           size={24}
-          style={{transform: [{rotate: '180deg'}]}}
+          style={{ transform: [{ rotate: '180deg' }] }}
         />
       </TouchableOpacity>
     </View>
